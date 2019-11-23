b025ed6434177e5aaad3b86b374d84226fe352fd
Angel Analoosen <an9an63@gmail.com>
2019-11-23 19:26:44 +0800

    Fix tagname style

--- full/script/get_tagname.py
+++ full/script/get_tagname.py
@@ -5,7 +5,7 @@ versions = sorted(version_got.items(), key=lambda x:(x[0], x[1]))
 
 lang_versions = []
 for lang, ver in versions:
-    lang_versions.append("{}-{}".format(lang, version_string(ver)))
+    lang_versions.append(lang + version_string(ver))
 
-tagname = str.join("_", lang_versions)
+tagname = str.join("-", lang_versions)
 print(tagname)
